e1c12c7fdb68b1cd8e05066fdb1ef9eb2f8315d4
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2024-07-20 15:53:39 +0545

    editor

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "tailwindcss";
+
 const { fontFamily } = require("tailwindcss/defaultTheme");
+
 const config = {
   darkMode: ["class"],
   content: [
@@ -15,9 +17,32 @@ const config = {
       padding: "2rem",
       screens: {
         "2xl": "1400px",
+        xs: "360px",
       },
     },
     extend: {
+      colors: {
+        blue: {
+          100: "#B4C6EE",
+          400: "#417BFF",
+          500: "#3371FF",
+        },
+        red: {
+          400: "#DD4F56",
+          500: "#DC4349",
+        },
+        dark: {
+          100: "#09111F",
+          200: "#0B1527",
+          300: "#0F1C34",
+          350: "#12213B",
+          400: "#27344D",
+          500: "#2E3D5B",
+        },
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -28,12 +53,13 @@ const config = {
           to: { height: "0" },
         },
       },
+      backgroundImage: {
+        doc: "url(/assets/images/doc.png)",
+        modal: "url(/assets/images/modal.png)",
+      },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-      },
-      fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
       },
     },
   },
