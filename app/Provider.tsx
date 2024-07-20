d86cc9618ac8e49c8d5157f42e91f9feef54a06f
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2024-07-20 22:25:22 +0545

    loader

---
+++ app/Provider.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React, { ReactNode } from "react";
+import {
+  ClientSideSuspense,
+  LiveblocksProvider,
+} from "@liveblocks/react/suspense";
+import Loader from "@/components/Loader";
+const Provider = ({ children }: { children: ReactNode }) => {
+  return (
+    <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+      <ClientSideSuspense
+        fallback={
+          <div>
+            <Loader />
+          </div>
+        }
+      >
+        {children}
+      </ClientSideSuspense>
+    </LiveblocksProvider>
+  );
+};
+
+export default Provider;
